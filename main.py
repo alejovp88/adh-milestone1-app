43b5ca81b1caec3b5435a1ed3400241cc6357af6
Alejandro Vargas <avargas@bulldogblockchain.com>
2024-02-28 16:48:12 -0400

    Read process update, html template to generate PDF

--- main.py
+++ main.py
@@ -1,62 +1,26 @@
-# import os.path
-#
-# from google.auth.transport.requests import Request
-# from google.oauth2.credentials import Credentials
-# from google_auth_oauthlib.flow import InstalledAppFlow
-# from googleapiclient.discovery import build
-# from googleapiclient.errors import HttpError
-#
-# SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
-#
-# SPREADSHEET_ID = "1tramso68YqQeJC4IVHHC4rOCZQ3Y7UbgJMHRytnGnZw"
-#
-# def main():
-#     credentials = None
-#     if os.path.exists("token.json"):
-#         credentials = credentials.from_authorized_user_file("token.json", SCOPES)
-#     if not credentials or not credentials.valid:
-#         if credentials and credentials.expired and credentials.refresh_token:
-#             credentials.refresh(Request())
-#         else:
-#             flow = InstalledAppFlow.from_client_secrets_file("ahd-milestone-creds.json", SCOPES)
-#             credentials = flow.run_local_server(port=0)
-#         with open("token.json", "w") as token:
-#             token.write(credentials.to_json())
-#
-#     try:
-#         service = build("sheets", "v4", credentials=credentials)
-#         sheets = service.spreadsheets()
-#
-#         for row in range(2, sheets.count()):
-#             num1 = sheets.values().get(spreadsheetId=SPREADSHEET_ID, range=f"Form Responses 1!A{row}").execute().get("values")[0][0]
-#     except HttpError as error:
-#         print(error)
-#
-#
-# if __name__ == '__main__':
-#     main()
+
+
 import time
 import gspread
 
 service_account = gspread.service_account(filename="ahd-milestone-creds.json")
 sheet = service_account.open("PDF Task")
 workSheet = sheet.worksheet("Form Responses 1")
-rowCount = int(workSheet.acell("CL1").value)
-rowValues = workSheet.row_values(rowCount)
+lastRowChecked = int(workSheet.acell("CL1").value)
+rowToCheck = lastRowChecked + 1
+rowValues = workSheet.row_values(rowToCheck)
 count = 0
 
 while True:
     try:
         while rowValues:
             print(rowValues)
-            rowCount = rowCount + 1
-            rowValues = workSheet.row_values(rowCount)
-            workSheet.update("CL1", rowCount)
-    except:
-        print("Something went wrong")
+
+            workSheet.update_acell('CL1', rowToCheck)
+            rowToCheck = rowToCheck + 1
+            rowValues = workSheet.row_values(rowToCheck)
+    except IOError as error:
+        print(error)
 
     time.sleep(10)#checks every 10 seconds
-    count = count + 1
-    if count == 2:
-        break
 
